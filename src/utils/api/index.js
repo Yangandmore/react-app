1281cc279dbcf52df6ea0a242225af300a54ccda
yangshiqu <635408602@qq.com>
2021-04-13 21:10:55 +0800

    eslint介入

---
+++ src/utils/api/index.js
@@ -0,0 +1,45 @@
+import frequest from './request';
+import config from '../../config';
+
+const hrequest = frequest;
+
+// 请求错误处理
+const responseError = (res) => {
+  let result = {
+    errcode: -1,
+  };
+  if (typeof res === 'string') {
+    result = Object.assign({}, result, { errmsg: res });
+  }
+  if (typeof res === 'object') {
+    result = {
+      errcode: res.body.errcode,
+      errmsg: res.body.errmsg,
+    };
+  }
+  return result;
+};
+
+// 请求入口
+const request = async (options, dispatch, getState) => {
+  const { url, endpoint, ...rest } = options;
+  const setUrl = config.url;
+  const opts = {
+    url: url || `${setUrl}${endpoint}`,
+    ...rest,
+  };
+
+  try {
+    const res = await hrequest(opts);
+
+    const { errcode, errmsg } = res.body;
+    if (errcode && errcode !== 0) {
+      return Promise.reject(res);
+    }
+    return Promise.resolve(res);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+};
+
+export { request, responseError, hrequest };
---
+++ src/utils/api/request.js
@@ -0,0 +1,156 @@
+// 请求数据
+const makeOptions = (url, options) => {
+  const defaultoptions = {
+    url: undefined,
+    method: 'GET',
+    qs: undefined,
+    body: undefined,
+    headers: undefined,
+    type: 'json',
+    contentType: 'application/json',
+    crossOrigin: true,
+    credentials: undefined,
+  };
+
+  let thisoptions = {};
+  if (options) {
+    thisoptions = options;
+    if (url) {
+      thisoptions.url = url;
+    }
+  } else {
+    thisoptions.url = url;
+  }
+  thisoptions = Object.assign({}, defaultoptions, thisoptions);
+
+  return thisoptions;
+};
+
+//
+const addQs = (url, qs) => {
+  let queryString = '';
+  let newUrl = url;
+  if (qs && typeof qs === 'object') {
+    for (const k of Object.keys(qs)) {
+      queryString += `&${k}=${qs[k]}`;
+    }
+    if (queryString.length > 0) {
+      if (url.split('?').length < 2) {
+        queryString = queryString.substring(1);
+      } else if (url.split('?')[1].length === 0) {
+        queryString = queryString.substring(1);
+      }
+    }
+
+    if (url.indexOf('?') === -1) {
+      newUrl = `${url}?${queryString}`;
+    } else {
+      newUrl = `${url}${queryString}`;
+    }
+  }
+
+  return newUrl;
+};
+
+//
+const parseJSON = (response) =>
+  response.text().then((text) => {
+    let rtn = {};
+    if (text) {
+      rtn = JSON.parse(text);
+    }
+    return rtn;
+  });
+
+// 网络请求
+const request = ({ url, ...options }) => {
+  const opts = makeOptions(url, options);
+  const { method, credentials, qs, type, mode } = opts;
+  let { body, headers } = opts;
+  let requestUrl = opts.url;
+  requestUrl = addQs(requestUrl, qs);
+
+  switch (type.toLowerCase()) {
+    case 'json':
+      body = JSON.stringify(body);
+      if (method.toLowerCase() === 'post' || method.toLowerCase() === 'put') {
+        headers = Object.assign({}, headers, {
+          'Content-Type': 'application/json',
+        });
+      }
+      break;
+    case 'form':
+      // 数据封装成fromData
+      let formData = new FormData();
+      for (var key in body) {
+        formData.append(key, body[key]);
+      }
+      body = formData;
+      break;
+    default:
+  }
+
+  return new Promise((resolve, reject) => {
+    let res = {};
+    /* global fetch:false */
+    fetch(requestUrl, {
+      method,
+      headers,
+      body,
+      credentials,
+      mode,
+    })
+      .then((response) => {
+        res = {
+          status: response.status,
+          statusText: response.statusText,
+        };
+        let data = {};
+        if (response.status !== 204) {
+          switch (type.toLowerCase()) {
+            case 'html':
+            case 'text':
+              data = response.text();
+              break;
+            case 'json':
+              data = parseJSON(response);
+              break;
+            case 'form':
+              data = parseJSON(response);
+              break;
+            case 'jpg':
+            case 'png':
+            case 'gif':
+            case 'img':
+              data = response.blob();
+              break;
+            default:
+              data = response.text();
+              break;
+          }
+        }
+        return data;
+      })
+      .then((data) => {
+        if (data.status === 0) {
+          // 正确请求
+          resolve({ body: data, parameter: opts.body });
+        } else if (data.status >= 500 && data.status <= 600) {
+          let err = {
+            errcode: data.status,
+            errmsg: data.msg,
+          };
+          reject(err);
+        } else {
+          let err = {
+            errcode: data.status,
+            errmsg: data.err,
+          };
+          reject(err);
+        }
+      })
+      .catch((err) => reject({ errcode: -1, errmsg: `${err}` }));
+  });
+};
+
+export default request;
